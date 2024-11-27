1b9eb272190734a3280bafbee52bd63f475ba6bb
Herve Desrosiers <herve_desrosiers@hotmail.com>
2024-11-27 11:42:17 -0500

    first dropdown

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-dropdown-carousel
 Odin project assignment at: https://www.theodinproject.com/lessons/node-path-javascript-dynamic-user-interface-interactions
+
+freeCodeCamp tutorial at: https://www.freecodecamp.org/news/how-to-build-a-dropdown-menu-with-javascript/
---
+++ script.js
@@ -0,0 +1,36 @@
+const dropdownBtn = document.getElementById('btn');
+const dropdownMenu = document.getElementById('dropdown');
+const toggleArrow = document.getElementById('arrow');
+
+// Toggle the show class on the dropdown element and rotate the dropdown arrow when the button is clicked.
+const toggleDropdown = () => {
+  dropdownMenu.classList.toggle('show');
+  toggleArrow.classList.toggle('arrow');
+};
+
+dropdownBtn.addEventListener('click', function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Add event listener to close the dropdown when users click outside
+document.addEventListener('click', (e) => {
+  const rect = dropdownMenu.getBoundingClientRect();
+  const isInDropdown =
+    e.clientX >= rect.left &&
+    e.clientX <= rect.right &&
+    e.clientY >= rect.top &&
+    e.clientY <= rect.bottom;
+
+  if (!isInDropdown && dropdownMenu.classList.contains('show')) {
+    toggleDropdown();
+  }
+});
+
+// Add click event listener to dropdown parent for event delegation
+dropdownMenu.addEventListener('click', (e) => {
+  const targetItem = e.target.closest('a');
+  if (targetItem && dropdownMenu.classList.contains('show')) {
+    toggleDropdown();
+  }
+});
